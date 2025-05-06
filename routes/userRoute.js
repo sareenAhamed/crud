2a6c66c5b14dfdc04347090a9058264d7f8e81f9
Sareen Ahamed <153348568+sareenAhamed@users.noreply.github.com>
2025-05-06 09:58:22 +0530

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,5 @@
-import express from "express"
-import { fetch } from "../controller/userController.js"
+import express from "express";
+import { fetch } from "../controller/userController.js";
 
 const route = express.Router();
 
